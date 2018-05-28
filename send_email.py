b9db38585c5d159494c7a8907eeb7b2fe09ce55e
import-yuefeng <zhuyuefeng0@outlook.com>
2018-05-28 12:15:08 +0800

    隐去个人信息

--- send_email.py
+++ send_email.py
@@ -1,28 +1,27 @@
-# POP3/SMTP服务 jekqrszpatkjbecb
+#!usr/bin/env python3
+# @File:send_email.py
+# @Date:2018/05/27
+# Author:Cat.1
 
 from email.mime.text import MIMEText
 import smtplib
+import config
 
-
-msg_from = "1069954477@qq.com"
-passwd = "jekqrszpatkjbecb"
-# 输入收件人地址:
-msg_to = "zhuyuefeng0@gmail.com"
-# 输入SMTP服务器地址:
-smtp_server = "smtp.qq.com"
-
-subject = "测试专用" 
-content = "厉害了我的哥"
-msg = MIMEText(content)
+msg_from       = config.getConfig("send_email", "msg_from")
+passwd         = config.getConfig("send_email", "passwd")
+msg_to         = config.getConfig("send_email", "msg_to")
+smtp_server    = config.getConfig("send_email", "smtp_server")
+subject        = "测试专用" 
+content        = "厉害了我的哥"
+msg            = MIMEText(content)
 msg['Subject'] = subject
-msg['From'] = msg_from
-msg['To'] = msg_to
-
-server = smtplib.SMTP_SSL("smtp.qq.com", 465)
-print(server.set_debuglevel(1))
+msg['From']    = msg_from
+msg['To']      = msg_to
+server         = smtplib.SMTP_SSL(smtp_server, 465)
 server.login(msg_from, passwd)
 server.sendmail(msg_from, msg_to, msg.as_string())
 server.quit()
+# print(server.set_debuglevel(1))
 
 
 
