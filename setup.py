93e154a92f3cb62c1ae61abab0252c503184c40f
root <root@zhuyuefeng.DHCP>
2018-07-07 21:15:35 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ import os
 import sys
 import platform
 
-path1 = "sudo ln -s " + os.path.abspath('.') + "/music.py " + "/usr/local/bin/pymusic" 
-path2 = "sudo ln -s " + os.path.abspath('.') + "/read_lyric.py " + "/usr/local/bin/read_lyric" 
+path1 = "sudo ln -s " + os.path.abspath('.') + "/pymusic/music.py " + "/usr/local/bin/pymusic" 
+path2 = "sudo ln -s " + os.path.abspath('.') + "/pymusic/read_lyric.py " + "/usr/local/bin/read_lyric" 
 
 try:
     os.system(path1)
@@ -30,7 +30,7 @@ else:
         os.system("brew install ffmpeg")
     elif platform == "linux":
         platform = {"1":"centos", "2":"ubuntu"}
-        platform = platform[print("请选择你的linux版本\n1.)centos\n2.)ubuntu")]
+        platform = platform[input("请选择你的linux版本\n1.)centos\n2.)ubuntu\n->")]
         if platform == "centos":
             os.system("sudo yum install mpg123")
             os.system("sudo yum install ffmpeg")
@@ -42,18 +42,63 @@ else:
 
 # 关于判断用户的Python版本中pip的构建问题
 pip = {"1":"pip", "2":"pip3", "3":"other"}
-pip = pip[print("请选择你的pip管理工具使用的哪一个名称\n1.)pip\n2.)pip3\n3.)other")]
-    if pip == "pip3":
-        os.system("pip3 install --upgrade pip")
-        os.system("pip3 install -r requirements.txt")
-    elif pip == "pip":
-        os.system("pip install --upgrade pip")
-        os.system("pip install -r requirements.txt")
-    else:
-        pip = print("键入你的pip管理工具的名称：")
-        os.system(pip + "install --upgrade pip")
-        os.system(pip + "install -r requirements.txt")
-    
+pip = pip[input("请选择你的pip管理工具使用的哪一个名称\n1.)pip\n2.)pip3\n3.)other\n->")]
+if pip == "pip3":
+    os.system("pip3 install --upgrade pip")
+    os.system("pip3 install -r requirements.txt")
+elif pip == "pip":
+    os.system("pip install --upgrade pip")
+    os.system("pip install -r requirements.txt")
+else:
+    pip = input("键入你的pip管理工具的名称：")
+    os.system(pip + "install --upgrade pip")
+    os.system(pip + "install -r requirements.txt")
+
+try:
+    os.system("touch error.log && touch access.log")
+    fp = open("config.ini", "w+")
+    fp.write("[uwsgi]\n\nsocket = 127.0.0.1:5051\nchdir = /root/listen-now/\nwsgi-file = main.py\ncallable = app\nprocesses = 4\nthreads = 2\nstats = 127.0.0.1:9191\n")
+    fp.close()
+    content = """
+    events {
+        worker_connections  1024;
+    }
+    http {
+        include       mime.types;    
+        default_type  application/octet-stream;    
+        sendfile        on;    
+        keepalive_timeout  65;
+
+        server {
+            listen 80;     
+            server_name 115.238.228.39;  
+            access_log  /root/Listen-now/access.log;   
+            error_log  /root/Listen-now/error.log;     
+
+            location / {
+
+                include        uwsgi_params;  
+
+                uwsgi_pass     127.0.0.1:5051;  
+
+
+                uwsgi_param UWSGI_PYHOME /root/Listen-now/venv;   
+
+                uwsgi_param UWSGI_CHDIR  /root/Listen-now/;  
+
+                uwsgi_param UWSGI_SCRIPT app:app;    
+                                                        
+
+            }
+        }
+    }
+    """
+    fp = open("nginx.conf", "w+")
+    fp.write(content)
+    fp.close()
+except:
+    pass
+else:
     print("[+]成功安装依赖环境!")
 
 
